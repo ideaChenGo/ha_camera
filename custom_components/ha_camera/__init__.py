206788d1b97c69281505215894ee5413a390834d
zhulu <shaonianzhentan@sina.cn>
2020-01-11 12:57:09 +0800

    up

--- custom_components/ha_camera/__init__.py
+++ custom_components/ha_camera/__init__.py
@@ -4,10 +4,16 @@ import urllib.parse
 import logging
 import aiohttp
 import async_timeout
+import asyncio
+import collections
+from urllib.parse import urlparse
 
 from homeassistant.helpers import template
 from homeassistant.components.http import HomeAssistantView
+
+from homeassistant.components.weblink import Link
 from aiohttp import web
+from aiohttp.hdrs import CACHE_CONTROL, CONTENT_TYPE
 from homeassistant.helpers.aiohttp_client import (
     async_aiohttp_proxy_web,
     async_get_clientsession,
@@ -15,18 +21,26 @@ from homeassistant.helpers.aiohttp_client import (
 
 _LOGGER = logging.getLogger(__name__)
 
+CACHE_LOCK = "lock"
+CACHE_IMAGES = "images"
+CACHE_MAXSIZE = "maxsize"
+CACHE_CONTENT = "content"
+ENTITY_IMAGE_CACHE = {CACHE_IMAGES: collections.OrderedDict(), CACHE_MAXSIZE: 16}
+
 DOMAIN = 'ha_camera'
 VERSION = '1.0'
 URL = '/'+DOMAIN+'-api-'+ str(uuid.uuid4())
 ROOT_PATH = URL + '/' + VERSION
 
-def setup(hass, config):
+def async_setup(hass, config):
     # 显示插件信息
     _LOGGER.info('''
 -------------------------------------------------------------------
     监控摄像头【作者QQ：635147515】
     
-    版本：''' + VERSION + '''    
+    版本：''' + VERSION + '''  
+
+    安全URL：''' + URL + '''      
         
     项目地址：https://github.com/shaonianzhentan/ha_camera
 -------------------------------------------------------------------''')
@@ -37,14 +51,18 @@ def setup(hass, config):
         hass.http.register_static_path(ROOT_PATH, local, False)
 
     # 摄像头IP
-    _ip = cfg.get('ip')
-    _user = cfg.get('user')
-    _password = cfg.get('password')
-    _snapshot = cfg.get('snapshot')
+    _ip = cfg.get('ip', '')
+    _user = cfg.get('user', '')
+    _password = cfg.get('password', '')
+    _snapshot = cfg.get('snapshot', '')
 
-    hass.data[DOMAIN] = HaCamera(hass, _ip,_user,_password,_snapshot)
+    hass.data[URL] = HaCamera(hass, _ip,_user,_password,_snapshot)
 
     hass.http.register_view(HassGateView)
+    
+    Link(hass, "摄像头", URL, "mdi:camcorder-box")
+    # 加载自定义卡片
+    hass.components.frontend.add_extra_js_url(hass, ROOT_PATH + '/ha-camera-panel.js')
     return True
 
 class HassGateView(HomeAssistantView):
@@ -56,8 +74,16 @@ class HassGateView(HomeAssistantView):
     async def get(self, request):
         # 这里进行重定向
         hass = request.app["hass"]
-        camera = hass.data[DOMAIN]
-        return await camera.async_camera_image()
+        camera = hass.data[URL]        
+        query = request.query
+        
+        if 'url' in query:
+            _url = query['url']
+            data, content_type = await camera.async_fetch_image(_url)
+            return web.Response(body=data, content_type=content_type, headers={})
+            
+        #headers = {CACHE_CONTROL: "max-age=3600"}
+        return self.json({})
         
 class HaCamera:
 
@@ -65,23 +91,44 @@ class HaCamera:
         self.ip = ip
         self.user = user
         self.password = password
-        self._still_image_url = snapshot
-        self._verify_ssl = False
+        self.snapshot = snapshot
         self.hass = hass
-    
-    async def async_camera_image(self):
-        """Return a still image response from the camera."""
+            
+    async def async_fetch_image(self, url):
+        """Fetch image.
+        Images are cached in memory (the images are typically 10-100kB in size).
+        """
+        hass = self.hass
+        cache_images = ENTITY_IMAGE_CACHE[CACHE_IMAGES]
+        cache_maxsize = ENTITY_IMAGE_CACHE[CACHE_MAXSIZE]
 
-        websession = async_get_clientsession(self.hass, verify_ssl=self._verify_ssl)
-        try:
-            with async_timeout.timeout(10):
-                response = await websession.get(self._still_image_url, auth=self._auth)
+        if urlparse(url).hostname is None:
+            url = hass.config.api.base_url + url
 
-                image = await response.read()
-                return image
+        if url not in cache_images:
+            cache_images[url] = {CACHE_LOCK: asyncio.Lock()}
 
-        except asyncio.TimeoutError:
-            _LOGGER.error("Timeout getting camera image")
+        async with cache_images[url][CACHE_LOCK]:
+            if CACHE_CONTENT in cache_images[url]:
+                return cache_images[url][CACHE_CONTENT]
 
-        except aiohttp.ClientError as err:
-            _LOGGER.error("Error getting new camera image: %s", err)
+            content, content_type = (None, None)
+            websession = async_get_clientsession(hass)
+            try:
+                with async_timeout.timeout(10):
+                    response = await websession.get(url)
+
+                    if response.status == 200:
+                        content = await response.read()
+                        content_type = response.headers.get(CONTENT_TYPE)
+                        if content_type:
+                            content_type = content_type.split(";")[0]
+                        cache_images[url][CACHE_CONTENT] = content, content_type
+
+            except asyncio.TimeoutError:
+                pass
+
+            while len(cache_images) > cache_maxsize:
+                cache_images.popitem(last=False)
+
+            return content, content_type
---
+++ custom_components/ha_camera/local/ha-camera-panel.js
@@ -0,0 +1,107 @@
+class HaCameraPanel extends HTMLElement {
+    constructor() {
+        super()
+        const shadow = this.attachShadow({ mode: 'open' });
+        const div = document.createElement('ha-card');
+        div.className = 'ha-camera-panel'
+        div.innerHTML = `
+        <div class="card-header">
+            <a class="name">
+                摄像头
+            </a>
+        </div>
+        
+        <div class="card-content">
+          <img id="snapshot" />
+		  <canvas id="video-canvas" class="hide"></canvas>
+        </div>		
+        <style>
+			#snapshot,
+			#video-canvas{width:100%;}		
+			.hide{display:none;}
+			.fullscreen{
+				position: absolute;
+				width: 98%;
+				top: 10px;
+				left: 1%;
+			}
+        </style>
+        `
+        shadow.appendChild(div)
+        this.shadow = shadow
+        this.$ = this.shadow.querySelector.bind(this.shadow)
+
+
+    }
+
+    toast(message) {
+        document.querySelector('home-assistant').dispatchEvent(new CustomEvent("hass-notification", { detail: { message } }))
+    }
+	
+	loadScript(url){
+		return new Promise((resolve)=>{
+			let { $ } = this
+			let id = url.replace(/[^0-9a-zA-Z]/g,'')
+			let script = $(`#${id}`)
+			if(script){
+				resolve()
+			}else{
+				script = document.createElement('script')
+				script.id = id
+				script.src = url
+				script.onload = ()=>{
+					resolve()
+				}
+				this.shadow.appendChild(script)
+			}			
+		})
+	}
+
+    get hass() {
+        return this._hass
+    }
+
+    set hass(hass) {
+        // console.log(hass)
+        this._hass = hass
+        let { states } = hass
+		let state = states['weblink.she_xiang_tou']
+		if(state){
+			let { $, config } = this
+			let { snapshot } = config
+			if(snapshot){
+				const canvas = $('#video-canvas');
+				const ss = $('#snapshot')
+				ss.src = `${state.state}?url=${snapshot}`
+				ss.onclick = ()=>{
+					ss.classList.toggle('hide')
+					canvas.classList.toggle('hide')
+					$('.ha-camera-panel').classList.toggle('fullscreen')
+					if(!window.haCameraPlayer){
+						window.haCameraPlayer = new JSMpeg.Player('ws://192.168.1.107:9999/', { canvas });
+					}else{
+						let cv = window.haCameraPlayer.options.canvas
+						// console.log(canvas, cv)
+						$('.card-content').replaceChild(cv, canvas)
+					}
+				}
+				$('.card-header .name').onclick = ()=>{
+					ss.onclick()
+				}
+				this.loadScript(`${state.state}/1.0/jsmpeg.min.js`).then(()=>{
+				})
+			}
+					
+		}
+    }
+
+    setConfig(config) {
+        this.config = config || {};
+    }
+
+    getCardSize() {
+        return 3
+    }
+}
+
+customElements.define('ha-camera-panel', HaCameraPanel);
